b7b6ed51fbc1dc11e3598319c1a091f2ea5767ef
anthony taranto <anthony@kato.im>
2013-11-25 12:41:45 -0800

    static lib build of MMDrawerController

---
+++ MMDrawerController/MMDrawerControllerTests/MMDrawerControllerTests.m
@@ -0,0 +1,34 @@
+//
+//  MMDrawerControllerTests.m
+//  MMDrawerControllerTests
+//
+//  Created by Anthony Taranto on 11/25/13.
+//  Copyright (c) 2013 Kato. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MMDrawerControllerTests : XCTestCase
+
+@end
+
+@implementation MMDrawerControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
